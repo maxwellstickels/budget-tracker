a251db6ee8f774de0fb13c31eb1fbd751ec57778
Max Stickels <maxwellstickels@gmail.com>
2021-07-27 22:50:45 -0700

    Replaced temporary webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
 const config = {
-  entry: "./public/index.js",
+  mode: "production",
+  entry: "/public/index.js",
   output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js"
+    path: __dirname + "/public/dist",
+    filename: "[name].bundle.js"
   },
-  mode: "development"
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Workout Tracker",
+      short_name: "Workouts",
+      description: "Track your workouts",
+      start_url: "/",
+      icons: [{
+        src: path.resolve("public/icons/icon-192x192.png"),
+        sizes: [192, 512],
+        destination: path.resolve("public/icons"),
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
 };
 module.exports = config;
